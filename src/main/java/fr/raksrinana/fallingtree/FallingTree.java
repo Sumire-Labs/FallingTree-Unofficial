adaf3cb59e6dcc7beaae3e1d78cd303c1d8461ff
s12kuma01 <amukat1201.ta@gmail.com>
2026-02-18 17:43:48 +0900

    Update ForgeDevEnv

--- src/main/java/fr/raksrinana/fallingtree/FallingTree.java
+++ src/main/java/fr/raksrinana/fallingtree/FallingTree.java
@@ -1,11 +1,11 @@
 package fr.raksrinana.fallingtree;
 
 import net.minecraftforge.fml.common.Mod;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = FallingTree.MOD_ID)
-public class FallingTree{
-	public static final String MOD_ID = "falling_tree";
-	public static final Logger LOGGER = LogManager.getLogManager().getLogger(MOD_ID);
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+public class FallingTree {
+	public static final String MOD_ID = Tags.MOD_ID;
+	public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 }
